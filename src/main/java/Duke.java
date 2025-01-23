2bd40c42394a0799070a0eda300de62cd3be517b
xiangrui <xiangrui02@gmail.com>
2025-01-24 01:21:16 +0800

    Revert "Rename, Greet, Exit"
    This reverts commit 4feca888b42ec48c63ad7b41e7aeeb806f6c9830.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-            + "Hello! I'm RuiBot\n"
-            + "What can I do for you?\n"
-            + "____________________________________________________________\n"
-            + "Bye. Hope to see you again soon!\n"
-            + "____________________________________________________________";
-
-        System.out.println(logo);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
