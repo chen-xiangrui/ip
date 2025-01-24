26cadb37874d42bce2a5b9c4bfe365b72240971f
xiangrui <xiangrui02@gmail.com>
2025-01-24 12:05:32 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
             + "Hello! I'm RuiBot\n"
             + "What can I do for you?\n"
-            + "____________________________________________________________\n"
-            + "Bye. Hope to see you again soon!\n"
-            + "____________________________________________________________";
+            + "____________________________________________________________\n";
 
         System.out.println(logo);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n"
+                        + input + "\n"
+                        + "____________________________________________________________\n");
+            }
+        }
     }
 }
